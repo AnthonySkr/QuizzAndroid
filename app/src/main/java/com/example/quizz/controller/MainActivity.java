59be7eb3268370fabf4ea9058de5105a17849c9f
Anthony <skrant44@gmail.com>
2025-07-11 11:05:47 +0200

    activity_main.xml et MainActivity created

--- app/src/main/java/com/example/quizz/controller/MainActivity.java
+++ app/src/main/java/com/example/quizz/controller/MainActivity.java
@@ -1,25 +1,41 @@
 package com.example.quizz.controller;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import com.example.quizz.R;
+import com.example.quizz.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        prefs = getSharedPreferences("quiz_prefs", Context.MODE_PRIVATE);
+
+        binding.btnStart.setOnClickListener(v -> {
+            String name = binding.etName.getText().toString().trim();
+            if (name.isEmpty()) {
+                Toast.makeText(this, getString(R.string.enter_name), Toast.LENGTH_SHORT).show();
+            } else {
+                prefs.edit().putString("user_name", name).apply();
+                Intent intent = new Intent(this, QuizActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
