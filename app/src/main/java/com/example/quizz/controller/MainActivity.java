3d0f35a47fb76a8483416f358c7bae1af3418f40
Anthony <skrant44@gmail.com>
2025-07-11 11:25:05 +0200

    MainActivity final

--- app/src/main/java/com/example/quizz/controller/MainActivity.java
+++ app/src/main/java/com/example/quizz/controller/MainActivity.java
@@ -5,14 +5,10 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
-import android.widget.Toast;
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import com.example.quizz.R;
 import com.example.quizz.databinding.ActivityMainBinding;
+import com.google.android.material.snackbar.Snackbar;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -30,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         binding.btnStart.setOnClickListener(v -> {
             String name = binding.etName.getText().toString().trim();
             if (name.isEmpty()) {
-                Toast.makeText(this, getString(R.string.enter_name), Toast.LENGTH_SHORT).show();
+                Snackbar.make(binding.getRoot(), getString(R.string.enter_name), Snackbar.LENGTH_SHORT).show();
             } else {
                 prefs.edit().putString("user_name", name).apply();
                 Intent intent = new Intent(this, QuizActivity.class);
